0eee52ded9a25ab1f8d3807b9b37fb53a06ed5cf
erebrus <erebrus@gmail.com>
2023-11-17 17:41:55 +0000

    recipe book ui

---
+++ godot/scenes/common/global.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var found_ingridients:Array[String]=["Chestnut Paste"]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func is_ingridient_found(str:String)->bool:
+	return str in found_ingridients
---
+++ godot/scenes/ui/recipes/ingridient.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name Ingridient
+
+@export var name:String
+@export_multiline var haiku:String
+@export var empty_board_png:Texture
+@export var written_board_png:Texture
+
+
+
+
